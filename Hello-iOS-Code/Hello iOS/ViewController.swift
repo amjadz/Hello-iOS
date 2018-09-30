342aaf37c0de73ffc2bdb4e2a630ef8f555c41e0
amjadz <amjadz@uw.edu>
2018-09-29 17:03:10 -0700

    Added my code Finished

---
+++ Hello-iOS-Code/Hello iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by iGuest on 9/29/18.
+//  Copyright © 2018 amjadz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
